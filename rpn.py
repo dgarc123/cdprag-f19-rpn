43a709c69ff9208821d91f2ab8f3b8f75d29b9a2
Daniela Garcia <dgarc@umich.edu>
2019-11-06 18:03:15 -0500

    from last lecture

--- rpn.py
+++ rpn.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-    pass
+    stack = list()
+    for token in arg.split():
+        if token == '+':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+
+        if len(stack) != 1:
+            raise Type
+
+        print(stack)
+    return stack.pop()
 
 def main():
     while True:
